e6ba01877c8d67e52fcf5566637bced9eab469bd
rushangbagada <rushang697@gmail.com>
2025-08-30 06:25:06 +0530

    minor error

--- server/server.js
+++ server/server.js
@@ -26,8 +26,8 @@ app.use((err, req, res, next) => {
   res.status(500).json({ message: "Something went wrong!" });
 });
 
-// 404 handler
-app.use("*", (req, res) => {
+// 404 handler - use a proper middleware function
+app.use((req, res) => {
   res.status(404).json({ message: "Route not found" });
 });
 
